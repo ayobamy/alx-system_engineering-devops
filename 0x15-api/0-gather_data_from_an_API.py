c61f86d8f4131df149854d92d4b7835d9a378b5f
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-17 20:57:43 +0000

    [update]: returns info about user's todo list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,11 +7,24 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        url = "https://jsonplaceholder.typicode.com"
-        customer_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = argv[1]
 
-        r = requests.get("{}/users/{}".format(url, customer_id)).json()
+    params = {"userId": user_id}
 
+    r = requests.get("{}/users/{}".format(url, user_id)).json()
+    user_todo = requests.get(url + "/todos", params).json()
+    
+    for data in user_todo:
+        if (data.get('completed')):
+            completed_tasks = data.get('title')
 
+    num_of_tasks = len(user_todo)
+    tasks_done = len(completed_tasks)
+    employee_name = user.get('name')
 
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          tasks_done, num_of_tasks))
+
+    for title in completed_tasks:
+        print("\t {}".format(title))
