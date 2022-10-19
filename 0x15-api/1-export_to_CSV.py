0b09998b2c4c76d8d551f2264c0b7df4ed38c210
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-19 20:55:42 +0000

    [fix]: PEP8 valiidation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,12 +19,11 @@ if __name__ == '__main__':
 
     line = []
     for info in todos:
-        line.append([employeeId, userName, info.get('completed'), \
-            info.get('title')])
+        line.append([employeeId, userName, info.get('completed'), info.get('title')])
 
     with open(fileName, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, delimiter = ',', quotechar='"', \
-            quoting=csv.QUOTE_ALL)
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
 
         for r in line:
             writer.writerow(r)
