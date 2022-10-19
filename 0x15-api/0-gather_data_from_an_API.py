28dd02ff8326cd72f667bfd6d93c02d869aa4596
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-19 20:11:28 +0000

    [fix]: fixed bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,20 +8,17 @@ from sys import argv
 
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com"
-    user_id = argv[1]
+    employeeId = argv[1]
 
-    params = {"userId": user_id}
-
-    r = requests.get("{}users/{}".format(url, user_id)).json()
-    todos = requests.get("{}todos?userId={}".format(url, user_id))
-    todos = todos.json()
-    employee_name = r.get('name')
+    employee = requests.get("{}/users/{}".format(url, employeeId)).json()
+    todos = requests.get(url + "/todos", params={"userId": employeeId}).json()
 
     completed_tasks = []
     for data in todos:
-        if (data.get('completed') is True):
-            completed_tasks.append(data)
+        if data.get('completed') is True:
+            completed_tasks.append(data.get('title'))
 
+    employee_name = employee.get('name')
     total_num_of_tasks = len(todos)
     num_of_tasks_done = len(completed_tasks)
 
@@ -29,4 +26,4 @@ if __name__ == '__main__':
           num_of_tasks_done, total_num_of_tasks))
 
     for title in completed_tasks:
-        print("\t {}".format(title.get("title")))
+        print("\t {}".format(title))
