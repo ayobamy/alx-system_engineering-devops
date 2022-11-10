b17c2f9ca692c01ae859cc7d431822405049abb4
ayobamy <ayobamyahmed@gmail.com>
2022-11-10 11:36:45 -0800

    [feat]:automating stacks fixing

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fixing stack
+exec { 'file limit':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
+}
